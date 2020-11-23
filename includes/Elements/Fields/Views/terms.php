db58c147fa339390d05961cf803e37a14fe5c132
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2020-11-23 19:38:23 -0300

    Fix to terms field type

--- includes/Elements/Fields/Views/terms.php
+++ includes/Elements/Fields/Views/terms.php
@@ -3,17 +3,26 @@ use AsasVirtuaisWPCore\Elements\Fields\Models\WordpressField;
 /** @var WordpressField $field */
 $taxonomies = get_taxonomies( [], 'objects' );
 $value = $field->get_value();
+av_show( $value, false, false );
 $value = is_array( $value ) ? $value : [];
 
-function av_terms_checkbox_loop( $terms, string $name, array $selected ) {
+function av_terms_checkbox_loop( $terms, string $name, string $taxonomy, array $selected ) {
 	?>
 		<ul>
 			<?php foreach( $terms as $term ): $children = get_terms( [ 'taxonomy' => $term->taxonomy, 'hide_empty' => false, 'parent' => $term->term_id ] ); ?>
 				<li>
 					<label multiple >
-						<input type="checkbox" value="<?= $term->term_id ?>" <?= in_array( $term->term_id, $selected ) ? 'checked' : null ; ?> name="<?= $name ?>[]" />
+						<input 
+							type="checkbox"
+							value="<?= $term->term_id ?>"
+							<?= in_array( 
+								$term->term_id,
+								( $selected[$taxonomy] ?? [] )
+							) ? 'checked' : null ; ?>
+							name="<?= $name ?>[<?= $taxonomy ?>][]"
+						/>
 						<b><?= $term->name ?></b>
-						<?php av_terms_checkbox_loop( $children, $name, $selected ); ?>
+						<?php av_terms_checkbox_loop( $children, $name, $term->taxonomy, $selected ); ?>
 					</label>
 				</li>
 			<?php endforeach; ?>
@@ -28,9 +37,9 @@ function av_terms_checkbox_loop( $terms, string $name, array $selected ) {
 		<li>
 			<?php foreach( $taxonomies as $key => $taxonomy ): $terms = get_terms( [ 'taxonomy' => $key, 'hide_empty' => false, 'parent' => 0 ] ); if ( ! $terms ) continue; ?>
 				<label multiple>
-					<input type="checkbox" <?= ( array_diff( array_column( $terms, 'term_id' ), $value ) ? '' : 'checked' ) ?> />
+					<input type="checkbox" <?= ( array_diff( array_column( $terms, 'term_id' ), array_values( $value[$key] ?? [] ) ) ? '' : 'checked' ) ?> />
 					<b><?= $taxonomy->label ?></b>
-					<?php av_terms_checkbox_loop( $terms, $field->name, $value ); ?>
+					<?php av_terms_checkbox_loop( $terms, $field->name, $key, $value ); ?>
 				</label>
 			<?php endforeach; ?>
 		</li>
